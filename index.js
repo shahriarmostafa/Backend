2f0f06de2c1ea672ad01456889b59c482da432a2
shahriarmostafa <sahirmiashab@gmail.com>
2025-01-05 01:57:20 +0600

    done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
     
     const adminList = client.db("adminListDB").collection("AddedUserDb");
 
@@ -93,9 +93,7 @@ async function run() {
       res.send(result)
     })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
